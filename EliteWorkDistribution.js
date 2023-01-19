6ee2de470c3c79dc09e60a14ae9b50189b78e506
Hassaan Ali <hassaanafzal789@gmail.com>
2023-01-19 18:33:04 +0500

    add empty line at the end of the line and remove js file

--- EliteWorkDistribution.js
+++
@@ -1,8 +0,0 @@
-Hassaan Ali and Mohammad Abdullah:
-We worked on Git, we created the repo and added our names on it, 
-than we created a new branch name add-requirement branch and added requirement.txt file,
-than we created a gitignore file and added EliteWorkDistribution file in it,
-
-Wajeeh Asim Huzaifa and Saif Shuja:
-They worked on JIRA Software also did the brainstorming for the project.
-
