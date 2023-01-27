6be9bf666f850ce2296a1f00a29f52a857ee16ef
hassaan ali <hassaan.ali@xloopdigital.com>
2023-01-27 16:20:54 +0500

    Extraction of Data from moisturemate and carbonSense

--- capstone_project/api.py
+++ capstone_project/api.py
@@ -1,35 +1,34 @@
-from fastapi import FastAPI, Request
-import uvicorn
 import json
 
-
+import uvicorn
+from fastapi import FastAPI, Request
 
 app = FastAPI()
 
 
 @app.post("/api/moisturemate")
-async def collect_moisture(request:Request):
+async def collect_moisture(request: Request):
     request_data = await request.json()
-    with open ("data.json", "w") as f:
+    with open("data.json", "w") as f:
         f.write(json.dumps(request_data))
         f.close()
     print(f"received : {request_data}")
 
-    return{"msg:ok"}
+    return {"msg:ok"}
+
 
 @app.post("/api/carbonsense")
-async def collect_carbon(request:Request):
+async def collect_carbon(request: Request):
     request_data = await request.json()
-    with open ("carbonsensor_data.json", "w") as f:
+    with open("carbonsensor_data.json", "w") as f:
         f.write(json.dumps(request_data))
         f.close()
     print(f"received : {request_data}")
-    return{"msg:ok"}
-
+    return {"msg:ok"}
 
 
 def run_app():
- 
+
     uvicorn.run(app, host="0.0.0.0", port=8000)
 
 
