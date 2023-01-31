330aa771473aa729efbfc32ed50a6e9aefe6d5c6
hassaan ali <hassaan.ali@xloopdigital.com>
2023-01-31 15:48:55 +0500

    Removed model and sensors directory, added team recourse, req.txt and pre commit file

--- data_extractions/extract_moisture_carbon_data/extract_moisture_carbon_data.py
+++ data_extractions/extract_moisture_carbon_data/extract_moisture_carbon_data.py
@@ -1,21 +1,31 @@
-from fastapi import FastAPI, Request
 import logging
+
 import uvicorn
+from fastapi import FastAPI, Request
+
 app = FastAPI()
 logger = logging.getLogger()
+
+
 @app.post("/api/moisturemate")
-async def collect_moisture(request:Request):
-    
+async def collect_moisture(request: Request):
+
     request_data = await request.json()
     logger.info(f"MoistureMate: {request_data}")
-   
-    return{"msg:ok"}
+
+    return {"msg:ok"}
+
+
 @app.post("/api/carbonsense")
-async def collect_carbon(request:Request):
+async def collect_carbon(request: Request):
     request_data = await request.json()
     logger.info(f"carbonsense: {request_data}")
-    return{"msg:ok"}
+    return {"msg:ok"}
+
+
 def run_app():
     uvicorn.run(app, host="0.0.0.0", port=8000)
+
+
 if __name__ == "__main__":
     run_app()
