4346765b516520888f2681b483010d9748422f7e
hassaan ali <hassaan.ali@xloopdigital.com>
2023-02-06 15:18:48 +0500

    Tests Added for mosituremate and carbonsense

---
+++ tests/etlservice/test_extract_moisture_carbon_data.py
@@ -0,0 +1,26 @@
+from fastapi.testclient import TestClient
+
+from caps.etlservice.extract_moisture_carbon_data import app
+
+client = TestClient(app)
+
+def test_collect_moisture_data_response_200():
+    response = client.post(
+        "/api/moisturemate",
+        json = {"test_key":"test_value"},
+    )
+    assert response.status_code == 200
+
+def test_collect_moisture_data_response_200():
+    response = client.post(
+        "/api/carbonsense",
+        json = {"test_key":"test_value"},
+    )
+    assert response.status_code == 200
+
+def test_invalid_endpoint_response_404():
+    response = client.post(
+        "/invalid_endpoint",
+        json={"test_key":"test_value"}
+    )
+    assert response.status_code == 404
---
+++ tests/etlservice/test_extract_moisture_carbon_data_with_logging.py
@@ -0,0 +1,35 @@
+from unittest import TestCase
+
+from fastapi.testclient import TestClient
+
+from caps.etlservice.extract_moisture_carbon_data import app
+
+client = TestClient(app)
+
+
+class TestExample(TestCase):
+    def test_moisturemate_data_response_200_and_logs(self):
+        with self.assertLogs() as captured:
+            response = client.post(
+                "/api/moisturemate",
+                json={"test_key": "test_value"},
+            )
+        assert response.status_code == 200
+        self.assertEqual(len(captured.records), 1)
+        self.assertEqual(
+            captured.records[0].getMessage(),
+            "MoistureMate: {'test_key': 'test_value'}",
+        )
+
+    def test_carbonsense_data_response_200_and_logs(self):
+        with self.assertLogs() as captured:
+            response = client.post(
+                "/api/carbonsense",
+                json={"test_key": "test_value"},
+            )
+        assert response.status_code == 200
+        self.assertEqual(len(captured.records), 1)
+        self.assertEqual(
+            captured.records[0].getMessage(),
+            "carbonsense: {'test_key': 'test_value'}",
+        )    
