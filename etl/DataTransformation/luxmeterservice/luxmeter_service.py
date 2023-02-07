5cf2a407541fef6280b9d5ffb469975f707641bb
Wajeeh Ul Hasan <wajeehulhasan2003>
2023-02-07 10:18:32 +0500

    added kafka producer function

---
+++ etl/DataTransformation/luxmeterservice/luxmeter_service.py
@@ -0,0 +1,53 @@
+import logging
+from fastapi import FastAPI
+import uvicorn
+import requests
+from fastapi_utils.tasks import repeat_every
+import requests
+import os
+import logging
+import json
+import pandas as pd
+from fastapi import FastAPI, Request
+from kafka import KafkaProducer
+
+app = FastAPI()
+logger = logging.getLogger()
+
+
+producer = KafkaProducer(
+    bootstrap_servers=os.environ.get("KAFKA_BOOTSTRAP_SERVERS"),
+    acks=1,
+    value_serializer=lambda v: json.dumps(v, default=str).encode("utf-8"),
+)
+
+
+
+room_id = ["kitchen", "bedroom", "bathroom", "living_room"]
+
+@app.on_event("startup")
+@repeat_every(seconds=60, wait_first=True)
+def lux_data():
+    for room in room_id:
+        url_template = f"http://sensorsmock:3000/api/luxmeter/{room}"
+        response = requests.get(url_template).json()
+        updated_response = dict((k, response[k]) for k in ['measurements']
+           if k in response)['measurements'][-1]
+        response['measurements'] = updated_response
+        try:
+            result = producer.send("luxmeter", value=updated_response)
+            logger.info(f"Received LuxMeter data{result}")
+        except:
+            logger.info(f"didn't run")
+
+def run_app():
+    logging.basicConfig(level=logging.INFO)
+    uvicorn.run(app, host="0.0.0.0", port=3007)
+if __name__ == "__main__":
+    run_app()
+
+
+
+
+
+
