abf1867d2de52ec8ceef8fd63753145c18f87a0d
Wajeeh Ul Hasan <wajeehulhasan2003>
2023-02-07 10:32:57 +0500

    added docker compose file

--- etl/DataTransformation/luxmeterservice/luxmeter_service.py
+++ etl/DataTransformation/luxmeterservice/luxmeter_service.py
@@ -1,14 +1,12 @@
-import logging
-from fastapi import FastAPI
-import uvicorn
-import requests
-from fastapi_utils.tasks import repeat_every
-import requests
-import os
-import logging
 import json
+import logging
+import os
+
 import pandas as pd
+import requests
+import uvicorn
 from fastapi import FastAPI, Request
+from fastapi_utils.tasks import repeat_every
 from kafka import KafkaProducer
 
 app = FastAPI()
@@ -22,8 +20,8 @@ producer = KafkaProducer(
 )
 
 
-
 room_id = ["kitchen", "bedroom", "bathroom", "living_room"]
+
 
 @app.on_event("startup")
 @repeat_every(seconds=60, wait_first=True)
@@ -31,23 +29,21 @@ def lux_data():
     for room in room_id:
         url_template = f"http://sensorsmock:3000/api/luxmeter/{room}"
         response = requests.get(url_template).json()
-        updated_response = dict((k, response[k]) for k in ['measurements']
-           if k in response)['measurements'][-1]
-        response['measurements'] = updated_response
+        updated_response = dict(
+            (k, response[k]) for k in ["measurements"] if k in response
+        )["measurements"][-1]
+        response["measurements"] = updated_response
         try:
             result = producer.send("luxmeter", value=updated_response)
             logger.info(f"Received LuxMeter data{result}")
         except:
             logger.info(f"didn't run")
 
+
 def run_app():
     logging.basicConfig(level=logging.INFO)
     uvicorn.run(app, host="0.0.0.0", port=3007)
+
+
 if __name__ == "__main__":
     run_app()
-
-
-
-
-
-
