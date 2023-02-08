47446262d4f1afac222b8802a2ed32058b93c3b8
mohammad.abdullah@xloopdigital.com <mohammad.abdullah@xloopdigital.com>
2023-02-08 08:55:04 +0500

    changes reviewed

--- etl/extract/luxmeterservice/luxmeter_service.py
+++ etl/extract/luxmeterservice/luxmeter_service.py
@@ -1,29 +1,21 @@
-import logging
 from fastapi import FastAPI
 import uvicorn
-import requests
 from fastapi_utils.tasks import repeat_every
+import logging
 app = FastAPI()
 logger = logging.getLogger()
- 
- 
-room_id = ["kitchen", "bedroom", "bathroom", "living_room"]
-
+room_ids = ["kitchen", "bedroom", "bathroom", "living_room"]
 @app.on_event("startup")
 @repeat_every(seconds=60, wait_first=True)
 def lux_data():
-    for rooms in room_id:
-        url_template = f"http://sensorsmock:3000/api/luxmeter/{rooms}"
+    for room in room_ids:
+        url_template = f"http://sensorsmock:3000/api/luxmeter/{room}"
         response = requests.get(url_template).json()
-        Updated_respose = dict((k, response[k]) for k in ['measurements']
-           if k in response)['measurements'][-1]
-        response['measurements'] = Updated_respose
-        logger.info(f"Received LuxMeter data:{Updated_respose}")
-
+        response_data = response.get('measurements')[-1] if 'measurements' in response else None
+        logger.info(f"Received LuxMeter data{response_data}")
+       
 def run_app():
     logging.basicConfig(level=logging.INFO)
-    uvicorn.run(app, host="0.0.0.0", port=3001)
+    uvicorn.run(app, host="0.0.0.0", port=3007)
 if __name__ == "__main__":
     run_app()
-
-
