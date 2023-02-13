8396215971ec27050cb83e802104a80a3acac478
hassaan ali <hassaan.ali@xloopdigital.com>
2023-02-13 11:43:39 +0500

    pre-commit run on luxmeter service

--- etl/extract/luxmeterservice/luxmeter_service.py
+++ etl/extract/luxmeterservice/luxmeter_service.py
@@ -1,10 +1,10 @@
-from fastapi import FastAPI
-from fastapi_utils.tasks import repeat_every
 import logging
 import os
+
 import requests
 import uvicorn
-import json
+from fastapi import FastAPI
+from fastapi_utils.tasks import repeat_every
 
 app = FastAPI()
 
@@ -12,23 +12,24 @@ logger = logging.getLogger()
 
 room_ids = ["kitchen", "bedroom", "bathroom", "living_room"]
 
+
 @app.on_event("startup")
 @repeat_every(seconds=60, wait_first=True)
 def lux_data():
-    
+
     for room in room_ids:
-        url_template=os.environ.get("LUXMETER_URL")
-        url= f"{url_template}{room}"
+        url_template = os.environ.get("LUXMETER_URL")
+        url = f"{url_template}{room}"
         response = requests.get(url).json()
         last_measurement = response["measurements"][-1]
-        final_data = {
-            "room": room,
-            "measurement": last_measurement
-        }
+        final_data = {"room": room, "measurement": last_measurement}
         logger.info(f"Received Luxmeter data: {final_data}")
-          
+
+
 def run_app():
     logging.basicConfig(level=logging.INFO)
     uvicorn.run(app, host="0.0.0.0", port=3007)
+
+
 if __name__ == "__main__":
     run_app()
