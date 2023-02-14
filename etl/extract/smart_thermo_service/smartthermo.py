edd244c778903bccfbc373f0591fa6fc05f13689
saifurrehmanshuja2211-025-khi-deg <saif.rehman@xloopdigital.com>
2023-02-14 23:56:10 +0500

    modified docker-compose.yml and add kafka_pyspark_streaming.ipynb

--- etl/extract/smart_thermo_service/smartthermo.py
+++ etl/extract/smart_thermo_service/smartthermo.py
@@ -1,9 +1,10 @@
-import boto3
 import json
+import logging
 import time
 from datetime import datetime
+
+import boto3
 import pandas as pd
-import logging
 from kafka import KafkaProducer
 
 logger = logging.getLogger()
@@ -12,42 +13,43 @@ logger = logging.getLogger()
 producer = KafkaProducer(
     bootstrap_servers=["kafka:29092"],
     acks=1,
-    value_serializer=lambda v: json.dumps(v, default=str).encode("utf-8")
+    value_serializer=lambda v: json.dumps(v, default=str).encode("utf-8"),
 )
 
-s3_client = boto3.client("s3",
+s3_client = boto3.client(
+    "s3",
     endpoint_url="http://minio:9000",
     aws_access_key_id="minioadmin",
-    aws_secret_access_key="minioadmin"
+    aws_secret_access_key="minioadmin",
 )
+
 
 def smartthermo():
     logger.info("Initiating smartthermoo function")
-    got_date=None
+    got_date = None
     while True:
         date = datetime.utcnow().replace(second=0, microsecond=0).isoformat()
-        
+
         bucket = "smart-thermo-sensor"
         key = f"smart_thermo/{date}.csv"
         time.sleep(10)
         try:
             if got_date != date:
                 obj = s3_client.get_object(Bucket=bucket, Key=key)
-                df = pd.read_csv(obj['Body'])
-                json_data = df.to_json(orient='records')
+                df = pd.read_csv(obj["Body"])
+                json_data = df.to_json(orient="records")
                 smart_data = json.loads(json_data)
                 for i in smart_data:
                     if "Unnamed: 0" in i:
                         del i["Unnamed: 0"]
                         logger.info(i)
                         producer.send("smartthermo", value=i)
-                got_date=date
+                got_date = date
         except Exception as e:
             logger.error(f"An error occured while processing data: {e}")
             pass
 
+
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
     smartthermo()
-
-
