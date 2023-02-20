fb44a984395c5bee976356330023e2bc62987301
saifurrehmanshuja2211-025-khi-deg <saif.rehman@xloopdigital.com>
2023-02-21 00:29:24 +0500

    modified docker-compose.yml

--- etl/extract/smart_thermo_service/smartthermo.py
+++ etl/extract/smart_thermo_service/smartthermo.py
@@ -1,55 +1,46 @@
 import json
 import logging
+import os
 import time
-from datetime import datetime
-
 import boto3
-import pandas as pd
 from kafka import KafkaProducer
 
 logger = logging.getLogger()
 
+endpoint = os.environ.get("ENDPOINT_URL")
+access_key = os.environ.get("AWS_ACCESS_KEY_ID")
+secret_key = os.environ.get("AWS_SECRET_ACCESS_KEY")
+bucket_name = os.environ.get("SMART_THERMO_BUCKET")
+kafka_bootstrap_server = os.environ.get("KAFKA_BOOTSTRAP_SERVER")
 
 producer = KafkaProducer(
-    bootstrap_servers=["kafka:29092"],
-    acks=1,
-    value_serializer=lambda v: json.dumps(v, default=str).encode("utf-8"),
+    bootstrap_servers=kafka_bootstrap_server,
+    value_serializer=lambda v: json.dumps(v).encode("utf-8"),
 )
 
-s3_client = boto3.client(
+s3 = boto3.resource(
     "s3",
-    endpoint_url="http://minio:9000",
-    aws_access_key_id="minioadmin",
-    aws_secret_access_key="minioadmin",
+    endpoint_url=endpoint,
+    aws_access_key_id=access_key,
+    aws_secret_access_key=secret_key,
 )
 
 
 def smartthermo():
-    logger.info("Initiating smartthermoo function")
-    got_date = None
-    while True:
-        date = datetime.utcnow().replace(second=0, microsecond=0).isoformat()
+    bucket = s3.Bucket(bucket_name)
 
-        bucket = "smart-thermo-sensor"
-        key = f"smart_thermo/{date}.csv"
-        time.sleep(10)
-        try:
-            if got_date != date:
-                obj = s3_client.get_object(Bucket=bucket, Key=key)
-                df = pd.read_csv(obj["Body"])
-                json_data = df.to_json(orient="records")
-                smart_data = json.loads(json_data)
-                for i in smart_data:
-                    if "Unnamed: 0" in i:
-                        del i["Unnamed: 0"]
-                        logger.info(i)
-                        producer.send("smartthermo", value=i)
-                got_date = date
-        except Exception as e:
-            logger.error(f"An error occured while processing data: {e}")
-            pass
+    objects = list(bucket.objects.all())
+    obj = list(filter(lambda o: o.key.endswith(".csv"), objects))[-1]
+    obj = s3.Object(bucket_name, obj.key)
+
+    csv_data = obj.get()["Body"].read().decode("utf-8")
+    logger.info(f"SmartThermo data read from bucket: {csv_data}")
+    kafka_producer_response = producer.send("smartthermo", value=csv_data)
+    logger.info(f"SmartThermo data sent to Kafka: {kafka_producer_response}")
 
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
-    smartthermo()
+    while True:
+        smartthermo()
+        time.sleep(60)
