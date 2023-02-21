386c491dbd3ad75da0f6f2bf752df75c5604325d
Muhammad Huzaifa Imtiaz <mohammad.huzaifa@xloopdigital.com>
2023-02-21 18:56:34 +0500

    Modified requirement.txt and transformation.py

--- etl/transformation/transformation.py
+++ etl/transformation/transformation.py
@@ -9,79 +9,84 @@ import pyspark.sql.types as T
 from pyspark.sql.functions import *
 
 spark = (
-    SparkSession.builder.master("local[1]")
+    SparkSession.builder.master("local[*]")
     .appName("Tutorial App")
     .config("spark.jars.packages", "org.apache.spark:spark-sql-kafka-0-10_2.12:3.3.1")
     .getOrCreate()
 )
     
-lux_meter= spark.readStream \
+lux_meter_reader= spark.readStream \
                           .format("kafka") \
                           .option("kafka.bootstrap.servers", "kafka:29092") \
-                          .option("subscribe", "luxmeter")
-carbon_sense= spark.readStream \
+                          .option("subscribe", "luxmeter")\
+                          .option("startingOffsets", "latest")
+carbon_sense_reader= spark.readStream \
                           .format("kafka") \
                           .option("kafka.bootstrap.servers", "kafka:29092") \
-                          .option("subscribe", "carbonsense")
-moisture_mate= spark.readStream \
+                          .option("subscribe", "carbonsense")\
+                          .option("startingOffsets", "latest")
+moisture_mate_reader= spark.readStream \
                           .format("kafka") \
                           .option("kafka.bootstrap.servers", "kafka:29092") \
-                          .option("subscribe", "moisturemate")
-smart_thermo= spark.readStream \
+                          .option("subscribe", "moisturemate")\
+                          .option("startingOffsets", "latest")
+smart_thermo_reader= spark.readStream \
                           .format("kafka") \
                           .option("kafka.bootstrap.servers", "kafka:29092") \
-                          .option("subscribe", "smartthermo")
-lux_meter=lux_meter.load()
-carbon_sense= carbon_sense.load()
-moisture_mate=moisture_mate.load()
-smart_thermo=smart_thermo.load()
+                          .option("subscribe", "smartthermo")\
+                          .option("startingOffsets", "latest")
+
+lux_meter_df = lux_meter_reader.load()
+carbon_sense_df = carbon_sense_reader.load()
+moisture_mate_df = moisture_mate_reader.load()
+smart_thermo_df = smart_thermo_reader.load()
 
 lux_meter_schema = StructType([
   StructField("room_id", StringType()),
   StructField("measurements", StructType([
-    StructField("timestamp", TimestampType()),
+    StructField("timestamp", T.TimestampType()),
     StructField("light_level", FloatType())
   ]))
 ])
 
 carbon_sense_schema = StructType([
-  StructField("timestamp", TimestampType()),
+  StructField("timestamp", T.TimestampType()),
   StructField("room_id", StringType()),
   StructField("co2", FloatType())
 ])
 
 moisture_mate_schema = StructType([
-  StructField("timestamp", TimestampType()),
+  StructField("timestamp", T.TimestampType()),
   StructField("room_id", StringType()),
   StructField("humidity", FloatType()),
   StructField("humidity_ratio", FloatType())
 ])
 
 smart_thermo_schema = StructType([
+  StructField("timestamp", T.TimestampType()),
   StructField("room_id", StringType()),
-    StructField("timestamp", TimestampType()),
-    StructField("temperature", FloatType())
-  ])
+  StructField("temperature", FloatType())
+])
 
-lux_meter= lux_meter.withColumn("message_content", F.from_json(F.col("value").cast("string"),lux_meter_schema))
-carbon_sense= carbon_sense.withColumn("message_content", F.from_json(F.col("value").cast("string"),carbon_sense_schema))
-moisture_mate= moisture_mate.withColumn("message_content", F.from_json(F.col("value").cast("string"),moisture_mate_schema))
-smart_thermo= smart_thermo.withColumn("message_content", F.from_json(F.col("value").cast("string"),smart_thermo_schema))
+lux_meter= lux_meter_df.withColumn("message_content", F.from_json(F.col("value").cast("string"),lux_meter_schema))
+carbon_sense= carbon_sense_df.withColumn("message_content", F.from_json(F.col("value").cast("string"),carbon_sense_schema))
+moisture_mate= moisture_mate_df.withColumn("message_content", F.from_json(F.col("value").cast("string"),moisture_mate_schema))
+smart_thermo= smart_thermo_df.withColumn("message_content", F.from_json(F.col("value").cast("string"),smart_thermo_schema))
 
-lux_meter = lux_meter.selectExpr("CAST(value AS STRING)")
-lux_meter = lux_meter.select(from_json(lux_meter.value, lux_meter_schema).alias("data"))
+lux_meter = lux_meter.withColumn("value", col("value").cast("string"))
+lux_meter = lux_meter.select(F.from_json(lux_meter.value, lux_meter_schema).alias("data"))
 lux_meter = lux_meter.select("data.*")
 
-carbon_sense = carbon_sense.selectExpr("CAST(value AS STRING)")
-carbon_sense = carbon_sense.select(from_json(carbon_sense.value, carbon_sense_schema).alias("data"))
+carbon_sense = carbon_sense.withColumn("value", col("value").cast("string"))
+carbon_sense = carbon_sense.select(F.from_json(carbon_sense.value, carbon_sense_schema).alias("data"))
 carbon_sense = carbon_sense.select("data.*")
 
-moisture_mate = moisture_mate.selectExpr("CAST(value AS STRING)", "timestamp")
-moisture_mate = moisture_mate.select(from_json(moisture_mate.value, moisture_mate_schema).alias("data"))
+moisture_mate = moisture_mate.withColumn("value", col("value").cast("string"))
+moisture_mate = moisture_mate.select(F.from_json(moisture_mate.value, moisture_mate_schema).alias("data"))
 moisture_mate = moisture_mate.select("data.*")
 
-smart_thermo = smart_thermo.selectExpr("CAST(value AS STRING)")
-smart_thermo = smart_thermo.select(from_json(smart_thermo.value, smart_thermo_schema).alias("data"))
+smart_thermo = smart_thermo.withColumn("value", col("value").cast("string"))
+smart_thermo = smart_thermo.select(F.from_json(smart_thermo.value, smart_thermo_schema).alias("data"))
 smart_thermo = smart_thermo.select("data.*")
 
 # Join the moisture_mate and carbon_sense DataFrames on "room_id" and "timestamp"
@@ -94,18 +99,14 @@ joined_df = joined_df.join(lux_meter_df, on=["room_id", "timestamp"])
 # Join the smart_thermo DataFrame with the existing DataFrame on "room_id" and "timestamp"
 joined_df = joined_df.join(smart_thermo, on=["room_id", "timestamp"])
 
-
 joined_df = joined_df.withColumnRenamed("timestamp", "date")
-
 joined_df = joined_df.withColumn("Temperature", (joined_df["temperature"] - 32) * 5/9)
-
 joined_df = joined_df.select("date", "Temperature","humidity", "light_level" ,"co2", "humidity_ratio", "room_id")
 
 joined_df = joined_df.withColumnRenamed('light_level', 'Light')
 joined_df = joined_df.withColumnRenamed('co2', 'CO2')
 joined_df = joined_df.withColumnRenamed('humidity', 'Humidity')
 joined_df = joined_df.withColumnRenamed('humidity_ratio', 'HumidityRatio')
-
 
 Joined_query = joined_df.writeStream \
   .outputMode("append") \
@@ -127,4 +128,3 @@ df_to_kafka_streaming_query = df_to_kafka_streaming.writeStream \
   .start()\
   .awaitTermination()
 df_to_kafka_streaming_query.status
-
